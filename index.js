eb152bfcbbeffecfc7b0d8319d145a8867ef4aca
Sohanur Rahman <49593164+sohanur-rahman0@users.noreply.github.com>
2020-06-25 02:54:34 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+for(var i = 0; i<7; i++){
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+      Sound(this.innerHTML);
+
+      btnanimation(this.innerHTML);
+
+    });
+}
+
+document.addEventListener("keypress", function (event) {
+  Sound(event.key);
+
+  btnanimation(event.key);
+});
+
+
+function Sound(key){
+  switch (key) {
+    case "w": let w = new Audio("sounds/tom-1.mp3");
+              w.play();
+              break;
+    case "a": let a = new Audio("sounds/tom-2.mp3");
+              a.play();
+              break;
+    case "s": let s = new Audio("sounds/tom-3.mp3");
+              s.play();
+              break;
+    case "d": let d = new Audio("sounds/tom-4.mp3");
+              d.play();
+              break;
+    case "j": let j = new Audio("sounds/snare.mp3");
+              j.play();
+              break;
+    case "k": let k = new Audio("sounds/crash.mp3");
+              k.play();
+              break;
+    case "l": let l = new Audio("sounds/kick-bass.mp3");
+              l.play();
+              break;
+    default: console.log("Wrong Key Press");
+
+  }
+}
+
+
+function btnanimation(key){
+  let active = "." + key;
+  document.querySelector(active).classList.add("pressed");
+  setTimeout(function (){
+    document.querySelector(active).classList.remove("pressed");
+  },100)
+}
